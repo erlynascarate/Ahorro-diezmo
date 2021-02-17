d08ccf72f36b28a5740e73ab2b630f0afd182c31
erlynascarate <erlyndavid@outlook.es>
2021-02-17 18:36:21 -0500

    Agrega un boton para que al precionarlo se active el codigo de js y se ajusta los estilos

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
-//gastoTotal es el 60%
-let gastoTotal = prompt('Escribe (en numero) el total de tus gastos');
+let pulsar = document.getElementById('pulsar');
 
-let sesentaX100 = parseInt(gastoTotal);
-let cuarentaX100 = (sesentaX100 / 3) * 2;
+pulsar.addEventListener('click', e=>{
+	ahorroDiezmo();
+})
 
-let ahorro = (cuarentaX100 / 4) * 3;
-let diezmo = cuarentaX100 / 4;
-
-let total = sesentaX100 + cuarentaX100;
-
-alert('Tienes esta cantidad en gastos: S/' + sesentaX100 + ', necesitas ganar S/' + total + ' porque tu ahorro tiene que ser S/' + ahorro + ' y tu diezmo de S/' + diezmo + '. Si consigues incrementar tus ingresos asta obtener S/' + total + ' podras pagar tus gastos , podras ahorrar lo necesario (almenos un 30%) y podras pagar tu diezmo sin que te falte dinero');
+function ahorroDiezmo(){
+	
+	//gastoTotal es el 60%
+	let gastoTotal = prompt('Escribe (en numero) el total de tus gastos');
+	
+		if (gastoTotal != ''){
+			let sesentaX100 = parseInt(gastoTotal);
+			let cuarentaX100 = (sesentaX100 / 3) * 2;
+			
+			let ahorro = (cuarentaX100 / 4) * 3;
+			let diezmo = cuarentaX100 / 4;
+			
+			let total = sesentaX100 + cuarentaX100;
+			
+			alert('Tienes esta cantidad en gastos: S/' + sesentaX100 + ', necesitas ganar S/' + total + ' porque tu ahorro tiene que ser S/' + ahorro + ' y tu diezmo de S/' + diezmo + '. Si consigues incrementar tus ingresos asta obtener S/' + total + ' podras pagar tus gastos , podras ahorrar lo necesario (almenos un 30%) y podras pagar tu diezmo sin que te falte dinero');
+		} else{
+			alert('Debes de ingresar algun numero. Ejemplo: 53')
+		}
+	
+}
