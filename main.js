b11f6ac6fcc392e30ba36d92aaf4292bf00718cc
erlynascarate <erlyndavid@outlook.es>
2021-02-18 11:59:12 -0500

    agrega un mensage si se pulso el boton de cancelar

--- main.js
+++ main.js
@@ -11,15 +11,22 @@ function ahorroDiezmo(){
 	
 		if (gastoTotal != '') {
 			
-			let sesentaX100 = parseInt(gastoTotal);
-			let cuarentaX100 = (sesentaX100 / 3) * 2;
+			if (gastoTotal != null){
+				
+				let sesentaX100 = parseInt(gastoTotal);
+				let cuarentaX100 = (sesentaX100 / 3) * 2;
+				
+				let ahorro = (cuarentaX100 / 4) * 3;
+				let diezmo = cuarentaX100 / 4;
 			
-			let ahorro = (cuarentaX100 / 4) * 3;
-			let diezmo = cuarentaX100 / 4;
+				let total = sesentaX100 + cuarentaX100;
 			
-			let total = sesentaX100 + cuarentaX100;
+				alert('Tienes esta cantidad en gastos: S/' + sesentaX100 + ', necesitas ganar S/' + total + ' porque tu ahorro tiene que ser S/' + ahorro + ' y tu diezmo de S/' + diezmo + '. Si consigues incrementar tus ingresos asta obtener S/' + total + ' podras pagar tus gastos , podras ahorrar lo necesario (almenos un 30%) y podras pagar tu diezmo sin que te falte dinero');
+			} else {
 			
-			alert('Tienes esta cantidad en gastos: S/' + sesentaX100 + ', necesitas ganar S/' + total + ' porque tu ahorro tiene que ser S/' + ahorro + ' y tu diezmo de S/' + diezmo + '. Si consigues incrementar tus ingresos asta obtener S/' + total + ' podras pagar tus gastos , podras ahorrar lo necesario (almenos un 30%) y podras pagar tu diezmo sin que te falte dinero');
+				alert('Debes de ingresar algun numero. Ejemplo: 53');
+			}
+			
 		} else{
 			
 			alert('Debes de ingresar algun numero. Ejemplo: 53');
